78a48adefdec39b197c683830b30cfa8fdbd644b
Melanie Petera <melanie.petera@clermont.inra.fr>
2015-05-12 09:12:34 +0200

    Changes in indentation

--- tablemerge_script.R
+++ tablemerge_script.R
@@ -33,50 +33,48 @@ tab.merge <- function(DM.name,meta.name,metype,output){
   # - output: output file's access
   
   
-  # Input --------------------------------------------------------------
-  
-  DM <- read.table(DM.name,header=TRUE,sep="\t",check.names=FALSE)
-  meta <- read.table(meta.name,header=TRUE,sep="\t",check.names=FALSE)
-  
-  # Table match check 
-  table.check <- match2(DM,meta,metype)
-  check.err(table.check)
-  
-  # StockID
-  meta.id <- stockID(DM,meta,metype)
-  DM<-meta.id$dataMatrix ; meta<-meta.id$Metadata ; meta.id<-meta.id$id.match
-  
-  
-  # Merging tables -----------------------------------------------------
-  
-  if(metype=="sample"){
-    ori.DM <- DM
-    rownames(DM) <- DM[,1]
-    DM <- DM[,-1]
-    DM <- t(DM)
-    DM <- data.frame(sample=row.names(DM),DM)
-    rownames(DM) <- NULL
-  }
-  
-  comb.data <- merge(x=meta,y=DM,by.x=1,by.y=1)
-  
-  
-  # Output -------------------------------------------------------------
-  
-  # Getting back original identifiers
-  if(metype=="sample"){
-    id.ori <- reproduceID(ori.DM,comb.data,metype,meta.id)
-  }else{
-    id.ori <- reproduceID(DM,comb.data,metype,meta.id)
-  }
-  comb.data <- id.ori$Metadata
-  
-  # Writing the table
-  write.table(comb.data,output,sep="\t",quote=FALSE,row.names=FALSE)
-  
-  
-  
-  
+# Input --------------------------------------------------------------
+
+DM <- read.table(DM.name,header=TRUE,sep="\t",check.names=FALSE)
+meta <- read.table(meta.name,header=TRUE,sep="\t",check.names=FALSE)
+
+# Table match check 
+table.check <- match2(DM,meta,metype)
+check.err(table.check)
+
+# StockID
+meta.id <- stockID(DM,meta,metype)
+DM<-meta.id$dataMatrix ; meta<-meta.id$Metadata ; meta.id<-meta.id$id.match
+
+
+# Merging tables -----------------------------------------------------
+
+if(metype=="sample"){
+  ori.DM <- DM
+  rownames(DM) <- DM[,1]
+  DM <- DM[,-1]
+  DM <- t(DM)
+  DM <- data.frame(sample=row.names(DM),DM)
+  rownames(DM) <- NULL
+}
+
+comb.data <- merge(x=meta,y=DM,by.x=1,by.y=1)
+
+
+# Output -------------------------------------------------------------
+
+# Getting back original identifiers
+if(metype=="sample"){
+  id.ori <- reproduceID(ori.DM,comb.data,metype,meta.id)
+}else{
+  id.ori <- reproduceID(DM,comb.data,metype,meta.id)
+}
+comb.data <- id.ori$Metadata
+
+# Writing the table
+write.table(comb.data,output,sep="\t",quote=FALSE,row.names=FALSE)
+
+
 } # End of tab.merge
 
 
